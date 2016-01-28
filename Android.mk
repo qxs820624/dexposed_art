de22319ae19bb506ebc24b0cd44ceeed7fffc055
lishaojie <super@joker.li>
2016-01-28 15:46:59 +0800

    x86 cpu passed

--- Android.mk
+++ Android.mk
@@ -9,11 +9,13 @@ LOCAL_SRC_FILES := dexposed.cpp
 LOCAL_SRC_FILES_arm := art_quick_dexposed_invoke_handler.arm.S
 LOCAL_SRC_FILES_arm64 := art_quick_dexposed_invoke_handler.arm64.S
 LOCAL_SRC_FILES_x86 := art_quick_dexposed_invoke_handler.x86.S
-LOCAL_SRC_FILES_x86_64 := art_quick_dexposed_invode_handler.x86_64.S
+LOCAL_SRC_FILES_x86_64 := art_quick_dexposed_invoke_handler.x86_64.S
 
-LOCAL_CPPFLAGS := -O3 -DNDEBUG
+LOCAL_LDFLAGS += -Wl,--no-warn-shared-textrel
 
-LOCAL_CFLAGS += -std=c++0x -O0 -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) -Wno-unused-parameter 
+LOCAL_CPPFLAGS := -O0 -DNDEBUG
+
+LOCAL_CFLAGS += -std=c++0x -O0 -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) -Wno-unused-parameter -fPIC
 
 LOCAL_C_INCLUDES := \
 	$(JNI_H_INCLUDE) \
@@ -23,9 +25,11 @@ LOCAL_C_INCLUDES := \
 	$(ART_C_INCLUDES) \
 	external/gtest/include \
 	external/valgrind/main/include \
-	external/valgrind/main/ \
+	external/valgrind/main
 
-include external/libcxx/libcxx.mk
+ifneq (6.0, $(PLATFORM_VERSION))
+	include external/libcxx/libcxx.mk
+endif
 
 ifeq (1,$(strip $(shell expr $(PLATFORM_SDK_VERSION) \>= 22)))
 	ifdef ART_IMT_SIZE
@@ -40,6 +44,10 @@ LOCAL_SHARED_LIBRARIES := libutils liblog libart libc++ libcutils libdl
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_MULTILIB := both
+ifeq (x86_64, $(TARGET_ARCH))
+	LOCAL_MULTILIB := 32
+else
+	LOCAL_MULTILIB := both
+endif
 
 include $(BUILD_SHARED_LIBRARY)
